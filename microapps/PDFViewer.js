ca39badab714a8a669e4f7805a896d432dc6dba0
Christian <christian.strunk2@gmail.com>
2018-06-26 21:19:25 +0200

    Possible to upload files to preview, PDF bugged

--- microapps/PDFViewer.js
+++ microapps/PDFViewer.js
@@ -3,11 +3,15 @@ var PDFViewer = class PDFViewer {
         this.win = pWin;
         this.name = "PDFViewer";
         this.win.classList.add(this.name);
-        this.html = "KUCHEN";
+        this.html = ""; 
         this.start();
     }
 
     start() {
         this.win.innerHTML = this.html;
+    }
+
+    display(data) {
+        this.win.innerHTML = "<iframe src=\"" + data + "\" height=\"100%\" width=\"100%\"></iframe>";
     }
 }
