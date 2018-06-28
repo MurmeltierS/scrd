57d485e22e6e7d24375630af12f2a09453b168c8
MurmeltierS <kevinsieger22@gmail.com>
2018-06-28 22:07:55 +0200

    made scrolling

--- microapps/PDFViewer.js
+++ microapps/PDFViewer.js
@@ -4,14 +4,27 @@ var PDFViewer = class PDFViewer {
         this.name = "PDFViewer";
         this.win.classList.add(this.name);
         this.html = ""; 
+        this.lastScroll;
         this.start();
     }
 
     start() {
         this.win.innerHTML = this.html;
+        setInterval(this.updatePv.bind(this), 1000);
+    }
+
+    scrollTo(rel){
+        this.lastScroll = rel;
+        var scroll = rel * this.win.clientHeight;
+        console.log("scroll:"+scroll);
+        this.win.querySelector("#swrap").scrollTop = scroll;
+    }
+
+    updatePv(){
+        remote.getGlobal('win').webContents.capturePage(function(e){s.call("setPv")(e.toDataURL(),this.lastScroll);}.bind(this));
     }
 
     display(data) {
-        this.win.innerHTML = "<webview src=\"" + data + "\" style=\"display: inline-flex; width: 100vw; height: 100vh;\" plugins></webview>";
+        this.win.innerHTML = "<div id=\"swrap\" style=\"height: 100vh; width: 100vw;overflow-y:scroll;\" ><webview src=\"" + data + "\" style=\"display: inline-flex; width: 100vw; height: 200vh\" plugins></webview></div>";
     }
 }
--- microapps/PDFViewerMobile.js
+++ microapps/PDFViewerMobile.js
@@ -1,30 +1,54 @@
-var PDFViewerMobile= class PDFViewerMobile {
+var PDFViewerMobile = class PDFViewerMobile {
     constructor(pWin) {
         this.win = pWin;
         this.name = "PDFViewerMobile";
         this.win.classList.add(this.name);
-        this.html = "<input id=\"uploadfile\" type=\"file\" /><input id=\"upload\" type=\"button\" value=\"clickme\" /><br><img id=\"pv\" src=\"\" height=\"500\" width=\"500\" alt=\"Image preview...\">";
+        this.html = "<input id=\"uploadfile\" type=\"file\" /><input id=\"upload\" type=\"button\" value=\"clickme\" /><br><div class=\"pvScroll\"><div class=\"fill\"><img id=\"pv\" src=\"\"  alt=\"Image preview...\"></div></div>";
         this.lastSearchTerm = "";
+        this.scrollTimeout = null;
         this.start();
     }
 
     start() {
         m.nextPage();
         this.win.innerHTML = this.html;
-        this.win.querySelector("#upload").addEventListener("click", this.startupload.bind(this), false); 
+        this.win.querySelector("#upload").addEventListener("click", this.startupload.bind(this), false);
+        this.win.querySelector(".pvScroll").addEventListener("scroll", this.handleScroll.bind(this), false);
     }
 
     back() {
         m.launch('Home');
     }
 
+
+    handleScroll(e) {
+        if (this.scrollTimeout) {
+            clearTimeout(this.scrollTimeout);
+        }
+        this.scrollTimeout = setTimeout(this.afterScroll.bind(this), 500);
+    }
+
+
+    afterScroll() {
+        console.log("you just scrolled");
+        var scroll = this.win.querySelector(".pvScroll").scrollTop / this.win.querySelector("#pv").height;
+        m.call("scrollTo")(scroll);
+    }
+
+    setPv(data, lastPos) {
+        document.getElementById("pv").src = data;
+        if (lastPos == this.win.querySelector(".pvScroll").scrollTop / this.win.querySelector("#pv").height) {
+            this.win.querySelector("#pv").style.top = this.win.querySelector(".pvScroll").scrollTop + "px";
+        }
+    }
+
     startupload() {
         var file = document.getElementById("uploadfile").files[0]
         var preview = document.getElementById("pv");;
         //var file    = document.querySelector('input[type=file]').files[0];
-        var reader  = new FileReader();
+        var reader = new FileReader();
 
-        reader.addEventListener("load", function () {
+        reader.addEventListener("load", function() {
             m.call("display")(reader.result);
         }, false);
 
